ff4e44f7ecde699207ef0b2f60a59a0476acc8ee
User <guest@example.com>
2025-09-15 16:27:03 +0300

    fix23

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -32,6 +32,8 @@ def test_postgres() -> None:
     conn = psycopg2.connect(
         host="127.0.0.1",
         port=5432,
+        user="github",
+        password="github",
         dbname="grab_test",
     )
 
